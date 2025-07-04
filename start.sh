02b3e081a74b4d3d8910b1f194ba5e1ae2360b42
MYKKUSH <148206548+MYKKUSH@users.noreply.github.com>
2025-07-04 22:58:29 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y ffmpeg
+mkdir -p bin
+cd bin
+curl -L -o ffmpeg.tar.xz https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz
+tar -xf ffmpeg.tar.xz --strip-components=1
+chmod +x ffmpeg
+cd ..
+
+export PATH=$PWD/bin:$PATH
 
 python bot.py
