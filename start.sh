84ec8587eac48516d987af76733005e4afac8be6
MYKKUSH <148206548+MYKKUSH@users.noreply.github.com>
2025-07-04 23:16:58 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Скачиваем ffmpeg (если нужно)
 mkdir -p bin
 cd bin
 curl -L -o ffmpeg.tar.xz https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz
@@ -8,8 +7,6 @@ tar -xf ffmpeg.tar.xz --strip-components=1
 chmod +x ffmpeg
 cd ..
 
-# Добавляем ffmpeg в PATH
 export PATH=$PWD/bin:$PATH
 
-# Запуск бота
 python bot.py
